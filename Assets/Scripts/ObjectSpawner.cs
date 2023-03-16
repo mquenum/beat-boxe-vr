960102f0e90702a945b7b5ab74fae0300906c70b
Mikayil Quenum <mquenum@users.noreply.github.com>
2023-03-16 17:13:54 +0100

    Tried multiscene workflow; XR Rig added to project; optimized scripts

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -6,22 +6,41 @@ public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _objectToSpawn;
     [SerializeField] private int _amountToSpawn;
+    public static Vector3 InitialTransform;
+    public static Quaternion InitialRotation;
     private List<GameObject> _pool;
+    private float _timer = 0;
 
     void Start()
     {
+        InitialTransform = transform.position;
+        InitialRotation = transform.rotation;
+
         // get the pool from the ObjectPooler
         _pool = ObjectPooler.SharedInstance.PooledObjects;
 
         for (int i = 0; i < _amountToSpawn; i++)
         {
             // instantiate object in variable
-            GameObject obj = Instantiate(_objectToSpawn);
+            GameObject obj = Instantiate(_objectToSpawn, InitialTransform, InitialRotation);
 
             // deactivate object
             obj.SetActive(false);
             // add object to the pool
             _pool.Add(obj);
+        }
+
+        ActivatePooledObj();
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= 5.0)
+        {
+            ActivatePooledObj();
+            _timer = 0;
         }
     }
 
