9ffd7ec3fafccf64167bc4cca4ddc7eb8db150cd
rg81893 <rguillorit@gmail.com>
2023-03-27 16:25:21 +0200

    activation des boutons
    activation des boutons, navigation menu principal / options

--- Assets/Scripts/MixerController.cs
+++ Assets/Scripts/MixerController.cs
@@ -2,14 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UIElements;
 
 public class MixerController : MonoBehaviour
 {
-    [SerializeField] private AudioMixer myAudioMixer;
-    
-    public void SetVolume(float sliderValue)
+    [SerializeField] private AudioMixerGroup myAudioMixer;
+
+    public void SetVolumeMaster(float sliderValue)
     {
         // valeur logarythmique de base 10 pour les DB
-        myAudioMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+        myAudioMixer.audioMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetVolumeBGM(float sliderValue) 
+    {
+        // valeur logarythmique de base 10 pour les DB
+        myAudioMixer.audioMixer.SetFloat("BGMVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetVolumeSFX(float sliderValue)
+    {
+        // valeur logarythmique de base 10 pour les DB
+        myAudioMixer.audioMixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
     }
 }
